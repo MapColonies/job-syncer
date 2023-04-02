e7a9f24ff56b492295c99e728f9f8130a2933fb4
Ron Nachum <ronnahom96@gmail.com>
2023-04-02 15:47:29 +0300

    unit tests

--- tests/mocks/jobManagerMock.ts
+++ tests/mocks/jobManagerMock.ts
@@ -1,4 +1,25 @@
+import { IJobResponse } from "@map-colonies/mc-priority-queue";
+import { IJobParameters, ITaskParameters } from "../../src/common/interfaces";
+
 export const jobManagerClientMock = {
   updateJob: jest.fn(),
   getJobs: jest.fn(),
 };
+
+export const getJobsMockResponse: IJobResponse<IJobParameters, ITaskParameters>[] = [
+  {
+    id: "1",
+    completedTasks: 10,
+    taskCount: 10
+  } as IJobResponse<IJobParameters, ITaskParameters>,
+  {
+    id: "2",
+    completedTasks: 5,
+    taskCount: 10
+  } as IJobResponse<IJobParameters, ITaskParameters>,
+  {
+    id: "3",
+    completedTasks: 10,
+    taskCount: 10
+  } as IJobResponse<IJobParameters, ITaskParameters>,
+];
--- tests/unit/jobSyncerManager/jobSyncerManager.spec.ts
+++ tests/unit/jobSyncerManager/jobSyncerManager.spec.ts
@@ -2,10 +2,16 @@ import { container } from 'tsyringe';
 import { getApp } from '../../../src/app';
 import { SERVICES } from '../../../src/common/constants';
 import { JobSyncerManager } from '../../../src/jobSyncerManager/jobSyncer';
-import { jobManagerClientMock } from '../../mocks/jobManagerMock';
+import { getJobsMockResponse, jobManagerClientMock } from '../../mocks/jobManagerMock';
 
 describe('jobSyncerManager', () => {
   let jobSyncerManager: JobSyncerManager;
+
+  global.fetch = jest.fn(async () =>
+    Promise.resolve({
+      json: async () => Promise.resolve({ test: 100 }),
+    }),
+  ) as jest.Mock;
 
   beforeAll(() => {
     getApp({
@@ -20,14 +26,24 @@ describe('jobSyncerManager', () => {
   });
 
   describe('progressJobs', () => {
-    it('When calling progressJobs it should update all the in progress jobs and update the catalog service', async () => {
+    it('When calling progressJobs without getting any jobs, then updateJob need not to be called and fetch', async () => {
       jobManagerClientMock.getJobs.mockResolvedValueOnce([]);
       jobManagerClientMock.updateJob.mockResolvedValue(undefined);
 
       await jobSyncerManager.progressJobs();
 
-      expect(jobManagerClientMock.getJobs).toHaveBeenCalledTimes(1);
-      expect(jobManagerClientMock.updateJob).toHaveBeenCalledTimes(0);
+      expect(jobManagerClientMock.updateJob).not.toHaveBeenCalled();
+      expect(jobManagerClientMock.getJobs).toHaveBeenCalled();
+    });
+
+    it('When calling progressJobs it should update all the in progress jobs and update the catalog service', async () => {
+      jobManagerClientMock.getJobs.mockResolvedValueOnce(getJobsMockResponse);
+      jobManagerClientMock.updateJob.mockResolvedValue(undefined);
+
+      await jobSyncerManager.progressJobs();
+
+      expect(jobManagerClientMock.getJobs).toHaveBeenCalled();
+      expect(jobManagerClientMock.updateJob).toHaveBeenCalledTimes(3);
     });
   });
 });
