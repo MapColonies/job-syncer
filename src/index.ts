ac0dcfa2adc0a951ca4dabe56869931eb48bef0e
netanelC <netanel153@gmail.com>
2024-05-21 10:19:06 +0300

    feat: add metrics

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 import 'reflect-metadata';
-import { Metrics } from '@map-colonies/telemetry';
-import { container } from 'tsyringe';
-import { tracing } from './common/tracing';
-import { SERVICES } from './common/constants';
 import { getApp } from './app';
 
-async function main(): Promise<void> {
+function main(): void {
   const app = getApp();
 
   app.run();
-
-  //stop tracing and metrics when app finish running
-  const metrics = container.resolve<Metrics>(SERVICES.METRICS);
-  await Promise.all([metrics.stop(), tracing.stop()]);
 }
 
 void main();
